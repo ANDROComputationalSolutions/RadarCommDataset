95fb3e6eacf4c5b7fd93ad50a6e7b1a7f091e779
Anu Jagannath <73130774+anujagann@users.noreply.github.com>
2020-10-30 23:35:34 -0400

    Update load_dataset.py

--- load_dataset.py
+++ load_dataset.py
@@ -3,7 +3,7 @@ import h5py
 dkeys = []  # labels of each entry in the h5
 W = []      # dataset container
 
-with h5py.File('ota_6mods8types_1e4.hdf5', 'r') as f:
+with h5py.File('RadComDynamic.hdf5', 'r') as f:
     for key in f.keys():
         dkeys.append(key)
         W.append(f[key][:])
